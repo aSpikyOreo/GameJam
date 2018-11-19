259b150160313fca0d09c1926209c3758b6cbedd
Josh <josh.hayford.jh@gmail.com>
2018-11-19 18:50:02 +0000

    Added vision to enemies

--- Test/Objects/Enemy.gd
+++ Test/Objects/Enemy.gd
@@ -4,22 +4,36 @@ export (float) var speed = 200
 var tileSize = 32
 var pos
 var player
+var tileMap
+var visionMap
 var cought = false
+export (PoolVector2Array) var points
+export (int) var vision = 2
+var currentPoint = 0
+var prevPos
 
 func _ready():
-	pos = Vector2(6, -6)
+	pos = pos_to_grid(position)
+	prevPos = pos
 	player = get_node("/root/Main/Player")
+	tileMap = get_node("/root/Main/Level")
+	visionMap = get_node("/root/Main/Vision")
 	pass
 
 func _process(delta):
 	var canMove = false
+	prevPos = pos_to_grid(position)
 	if (!cought):
 		canMove = move_enemy(delta)
-	if (canMove && pos == Vector2(6, -6)):
-		pos = Vector2(6, 4)
-	elif (canMove && pos == Vector2(6, 4)):
-		pos = Vector2(6, -6)
-	if (player.pos == pos_to_grid(position)):
+	if (canMove):
+		var prevPoint = pos
+		currentPoint += 1
+		if (currentPoint >= points.size()):
+			currentPoint = 0
+		pos = points[currentPoint]
+	if (prevPos != pos_to_grid(pos)):
+		update_vision(prevPos)
+	if (can_see_player()):
 		cought = true
 	pass
 
@@ -32,6 +46,57 @@ func move_enemy(delta):
 	else:
 		position += moveVector * delta * speed
 		return false
+
+func can_see_player():
+	var currentPos = pos_to_grid(position)
+	var see = false
+	if (currentPos == player.pos):
+		see = true
+	var i = 1
+	while (i <= vision && !see && player.valid_move(currentPos.x - i, currentPos.y)):
+		currentPos + Vector2(-i, 0)
+		see = currentPos + Vector2(-i, 0) == player.pos
+		i += 1
+	i = 1
+	while (i <= vision && !see && player.valid_move(currentPos.x + i, currentPos.y)):
+		see = currentPos + Vector2(i, 0) == player.pos
+		i += 1
+	i = 1
+	while (i <= vision && !see && player.valid_move(currentPos.x, currentPos.y - i)):
+		see = currentPos + Vector2(0, -i) == player.pos
+		i += 1
+	i = 1
+	while (i <= vision && !see && player.valid_move(currentPos.x, currentPos.y + i)):
+		see = currentPos + Vector2(0, i) == player.pos
+		i += 1
+	return see
+
+func update_vision(prevPos):
+	var currentPos = pos_to_grid(position)
+	var i = 1
+	while (i <= vision):
+		visionMap.set_cellv(prevPos + Vector2(-i, 0), -1)
+		visionMap.set_cellv(prevPos + Vector2(i, 0), -1)
+		visionMap.set_cellv(prevPos + Vector2(0, -i), -1)
+		visionMap.set_cellv(prevPos + Vector2(0, i), -1)
+		i += 1
+	i = 1
+	visionMap.set_cellv(currentPos, 0)
+	while (i <= vision && player.valid_move(currentPos.x - i, currentPos.y)):
+		visionMap.set_cellv(currentPos + Vector2(-i, 0), 0)
+		i += 1
+	i = 1
+	while (i <= vision && player.valid_move(currentPos.x + i, currentPos.y)):
+		visionMap.set_cellv(currentPos + Vector2(i, 0), 0)
+		i += 1
+	i = 1
+	while (i <= vision && player.valid_move(currentPos.x, currentPos.y - i)):
+		visionMap.set_cellv(currentPos + Vector2(0, -i), 0)
+		i += 1
+	i = 1
+	while (i <= vision && player.valid_move(currentPos.x, currentPos.y + i)):
+		visionMap.set_cellv(currentPos + Vector2(0, i), 0)
+		i += 1
 
 func grid_to_pos(grid):
 	return grid * 32 + Vector2(tileSize/2, tileSize/2)
